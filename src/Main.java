8bc2752d960499d864b9cfa8b1608945c4367c37
Tatiana Rudikova <tatjana_tv@list.ru>
2022-09-28 16:49:55 +0300

    added project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int translation = 1100;
+        int bonus = 0;
+
+        if (translation > 1000){
+            bonus = translation / 100;
+        }
+
+        int amount = account + translation + bonus;
+
+        System.out.println("Итоговая сумма по счету клиента " + amount);
+    }
+}
